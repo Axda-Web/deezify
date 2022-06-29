fc88139d51cd47493ad405351df050ad22a356c5
Axda <axda.web@gmail.com>
2022-06-29 07:32:16 +0200

    Set up styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { ThemeProvider } from 'styled-components'
+
+import GlobalStyles from './components/styles/Global'
+
+const theme = {
+  colors: {
+    body: '#FFF'
+  },
+  bgColors: {
+    body: '#000'
+  },
+  fonts: {
+    body: `'Roboto', sans-serif`
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <h1>App</h1>
+      </>
+    </ThemeProvider>
   );
 }
 
